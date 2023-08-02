ab0e5c94c1983cf98e31873b6398292d08d64dcc
Yusuke Kawamoto <novogrammer@gmail.com>
2023-08-02 17:19:09 +0900

    numpyとcv2を追加

--- jpeg_filter_prototype/receiver.py
+++ jpeg_filter_prototype/receiver.py
@@ -1,4 +1,6 @@
 import socket
+import cv2
+import numpy
 
 MY_IP = "127.0.0.1"
 MY_PORT = 5005
@@ -13,8 +15,14 @@ print(f"Connected by {addr}")
 
 data = conn.recv(65535) # バッファサイズは適切に調整してください
 
-# 画像を保存するなどの処理
-with open('received_image.jpg', 'wb') as f:
-  f.write(data)
+img_buf=numpy.frombuffer(data,dtype=numpy.uint8)
+img=cv2.imdecode(img_buf,cv2.IMREAD_COLOR)
+cv2.imshow("imdecode",img)
+cv2.waitKey(0)
+
+# # 画像を保存するなどの処理
+# with open('received_image.jpg', 'wb') as f:
+#   f.write(data)
+
 
 print("Received")
