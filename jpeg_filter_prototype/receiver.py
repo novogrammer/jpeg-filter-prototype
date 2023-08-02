04c4a9e5486e617747d4da624163984673bde178
Yusuke Kawamoto <novogrammer@gmail.com>
2023-08-02 17:52:40 +0900

    構造を変更、cv2.waitKey(0)は不要かも。

--- jpeg_filter_prototype/receiver.py
+++ jpeg_filter_prototype/receiver.py
@@ -10,19 +10,23 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((MY_IP, MY_PORT))
 
 sock.listen(1)
-conn, addr = sock.accept()
-print(f"Connected by {addr}")
 
-data = conn.recv(65535) # バッファサイズは適切に調整してください
+print("Waiting for connection...")
 
-img_buf=numpy.frombuffer(data,dtype=numpy.uint8)
-img=cv2.imdecode(img_buf,cv2.IMREAD_COLOR)
-cv2.imshow("imdecode",img)
-cv2.waitKey(0)
+while True:
+  conn, addr = sock.accept()
+  print(f"Connected by {addr}")
 
-# # 画像を保存するなどの処理
-# with open('received_image.jpg', 'wb') as f:
-#   f.write(data)
+  while True:
+    data = conn.recv(65535) # バッファサイズは適切に調整してください
+    if not data:
+      print("Client disconnected.")
+      conn.close()
+      break
+    print("Received.")
 
-
-print("Received")
+    img_buf=numpy.frombuffer(data,dtype=numpy.uint8)
+    img=cv2.imdecode(img_buf,cv2.IMREAD_COLOR)
+    cv2.imshow("imdecode",img)
+    cv2.waitKey(0)
+  print("Waiting for next connection...")
