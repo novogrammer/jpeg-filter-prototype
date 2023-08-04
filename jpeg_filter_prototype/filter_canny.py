8f2bbe4f6fb8cff6e6a709b77354772ff703547a
Yusuke Kawamoto <novogrammer@gmail.com>
2023-08-04 20:43:01 +0900

    filter_common.pyに共通ロジックを寄せる。

---
+++ jpeg_filter_prototype/filter_canny.py
@@ -0,0 +1,14 @@
+
+import cv2
+from cv2 import UMat
+
+from filter_common import run
+
+
+def callback(img_before:UMat)->UMat:
+  img_gray = cv2.cvtColor(img_before, cv2.COLOR_BGR2GRAY)
+  img_after = cv2.Canny(img_gray, 100, 200)
+  return img_after
+
+
+run(callback)
