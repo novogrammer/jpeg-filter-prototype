60d7e326074e71959dc3d2b63d6d38311f4eec27
Yusuke Kawamoto <novogrammer@gmail.com>
2023-09-11 22:46:18 +0900

    senderの接続リトライ処理

--- jpeg_filter_prototype/sender.py
+++ jpeg_filter_prototype/sender.py
@@ -24,36 +24,43 @@ print(f"JPEG_QUALITY: {JPEG_QUALITY}")
 print(f"IMAGE_WIDTH: {IMAGE_WIDTH}")
 print(f"FPS: {FPS}")
 
+if FROM_FILE:
+  with open('sending_image.jpg', 'rb') as f:
+    data = f.read()
+else:
+  capture=cv2.VideoCapture(0)
 
+while True:
+  try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock_for_send:
+      sock_for_send.connect((MY_IP, MY_PORT))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock_for_send:
-  sock_for_send.connect((MY_IP, MY_PORT))
+      if FROM_FILE:
+        send_image(sock_for_send,data)
+        print("Sent.")
+      else:
+        previous_time=time.perf_counter()
+        frame_width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        while capture.isOpened():
+          before_sleep_time=time.perf_counter()
+          time.sleep(max(0,SPF - (before_sleep_time - previous_time)))
+          previous_time=time.perf_counter()
+          result_read,frame=capture.read()
+          if not result_read:
+            print("not result_read")
+            continue
+          resized_frame=cv2.resize(frame, (IMAGE_WIDTH,int(IMAGE_WIDTH / frame_width * frame_height)))
+          result_encode,encoded=cv2.imencode(".jpg", resized_frame, (cv2.IMWRITE_JPEG_QUALITY, JPEG_QUALITY))
+          if not result_encode:
+            print("not result_encode")
+            continue
+          data=encoded.tobytes()
+          send_image(sock_for_send,data)
+          # print("Sent.")
+        break
+  except socket.error as e:
+    print(e)
+    print("retry connect")
+    time.sleep(1)
 
-  if FROM_FILE:
-    with open('sending_image.jpg', 'rb') as f:
-      data = f.read()
-    send_image(sock_for_send,data)
-  else:
-    capture=cv2.VideoCapture(0)
-    previous_time=time.perf_counter()
-    frame_width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    while capture.isOpened():
-      before_sleep_time=time.perf_counter()
-      time.sleep(max(0,SPF - (before_sleep_time - previous_time)))
-      previous_time=time.perf_counter()
-      result_read,frame=capture.read()
-      if not result_read:
-        print("not result_read")
-        continue
-      resized_frame=cv2.resize(frame, (IMAGE_WIDTH,int(IMAGE_WIDTH / frame_width * frame_height)))
-      result_encode,encoded=cv2.imencode(".jpg", resized_frame, (cv2.IMWRITE_JPEG_QUALITY, JPEG_QUALITY))
-      if not result_encode:
-        print("not result_encode")
-        continue
-      data=encoded.tobytes()
-      send_image(sock_for_send,data)
-    capture.release()
-
-
-print("Sent.")
